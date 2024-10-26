feedae8cb9e192a858d1f05407bf0929c1434dec
muritador5050 <muritador5050@gmail.com>
2024-10-26 14:52:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { QRCodeCanvas } from "qrcode.react";
+const intialValue = {
+  email: "",
+  subject: "",
+  message: "",
+};
 function App() {
+  const [datas, setDatas] = useState(intialValue);
+  function handleOnChange(e) {
+    setDatas({ ...datas, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(datas);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Qr Code Generator</h1>
       </header>
+      <div className="App">
+        <form className="email-form" onSubmit={handleSubmit}>
+          <h3>Email Qr Code</h3>
+          <label>
+            <h3>Email:</h3>
+            <input
+              placeholder="Your Email"
+              type="email"
+              name="email"
+              value={datas.email || ""}
+              onChange={handleOnChange}
+           
+            />
+          </label>{" "}
+          <br /> <br />
+          <label>
+            <h3>Subject:</h3>
+            <input
+              placeholder="Enter Email Subject"
+              type="text"
+              name="subject"
+              value={datas.subject || ""}
+              onChange={handleOnChange}
+            />
+          </label>{" "}
+          <br />
+          <br />
+          <label>
+            <h3>Message:</h3>
+            <textarea
+              placeholder="Enter Your Message"
+              name="message"
+              value={datas.message || ""}
+              onChange={handleOnChange}
+            />
+          </label>
+          <br />
+          <br />
+          <button type="submit" className="submit">
+            GENERATE QR CODE
+          </button>
+        </form>
+        <div className="qrcode">
+          <h4>Scan Me</h4>
+          <QRCodeCanvas
+            value={"https://github.com/muritador5050/crud_operation"}
+            size={200}
+            minVersion={3}
+          />
+        </div>
+      </div>
     </div>
   );
 }
