104b9d2e906a23f98d210b50838f6c86a17b5fa2
muritador5050 <muritador5050@gmail.com>
2025-02-16 18:36:38 +0100

    i change all the page setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react";
-import "./App.css";
+import { useRef, useState } from 'react';
+import './App.css';
 const intialValue = {
-  data: "",
-  color: "",
+  data: '',
+  color: '',
 };
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
   }
 
   const handleOnfocus = () => {
-    inputRef.current.value = "";
+    inputRef.current.value = '';
   };
 
   async function handleSubmit(e) {
@@ -33,59 +33,57 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <div className='container'>
       <header>
-        <h1>Qr Code Generator</h1>
+        <h1 className='heading-text'>Qr Code Generator</h1>
       </header>
-      <div className="App">
-        <form className="email-form" onSubmit={handleSubmit}>
-          <h3>
-            GENERATE HERE <small className="entity-arrow-down">&darr;</small>{" "}
+      <div className='flex-container'>
+        <div className='qrcode'>
+          {qrimage ? (
+            <div className='scanner'>
+              <img src={qrimage} alt='qr code' />
+              <h4 className='scan-me'>Scan Me</h4>
+            </div>
+          ) : (
+            <div className='rotate'>
+              <h3 className='no-qr-yet'>No qrcode yet!</h3>
+            </div>
+          )}
+        </div>
+        <div className='url-input'>
+          <h3 className='generate'>
+            GENERATE HERE <small className='entity-arrow-down'>&darr;</small>{' '}
           </h3>
-          <label>
-            <h3>Data:</h3>
+          <div className='input-group'>
             <input
-              placeholder="Enter Url, Email e.t.c"
-              type="text"
-              name="data"
-              value={datas.data || ""}
+              type='text'
+              name='data'
+              value={datas.data || ''}
               onChange={handleOnChange}
               ref={inputRef}
               onFocus={handleOnfocus}
             />
-          </label>{" "}
-          <br /> <br />
-          <label>
-            <h3>Color:</h3>
+            <label>Enter Url, Email e.t.c</label>
+          </div>
+          <div className='select-group'>
             <select
-              name="color"
-              value={datas.color || ""}
+              name='color'
+              value={datas.color || ''}
               onChange={handleOnChange}
             >
-              <option value="000000">Black</option>
-              <option value="FFFF00">yellow</option>
-              <option value="0000FF">Blue</option>
-              <option value="008000">Green</option>
-              <option value="800080">Purple</option>
-              <option value="FF0000">Red</option>
-              <option value="00FFFF">Aqua</option>
+              <option value='000000'>Black</option>
+              <option value='FFFF00'>yellow</option>
+              <option value='0000FF'>Blue</option>
+              <option value='008000'>Green</option>
+              <option value='800080'>Purple</option>
+              <option value='FF0000'>Red</option>
+              <option value='00FFFF'>Aqua</option>
             </select>
-          </label>{" "}
-          <br />
-          <br />
-          <button type="submit" className="submit">
+          </div>
+
+          <button onClick={handleSubmit} className='submit'>
             GENERATE QR-CODE
           </button>
-        </form>
-        <div className="qrcode">
-          {qrimage ? (
-            <frames className="scanner">
-              <img src={qrimage} alt="qr code" /> <br /> <br />
-              <h4 className="scan-me">Scan Me</h4>
-            </frames>
-          ) : (
-            "Display Qr code here"
-          )}
         </div>
       </div>
     </div>
